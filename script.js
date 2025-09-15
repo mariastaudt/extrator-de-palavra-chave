8b166a37f1e4673510ec154eb93542a716576513
mariastaudt <maria.staudt@escola.pr.gov.br>
2025-09-15 09:01:27 -0300

    df

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ function processa Texto (texto) {
     let palavras = texto.split(/\P{L}+/u);
     
     let frequencias = [];
-    for(let i of palavras) {
-        frequencias[i] = 0;
-        for (let j of palavras) {
-            if (i == j) {
-                frequencias [i]++;
+    for(let i in palavras) {
+        frequencias [i] = 0;
+        for (let jin palavras) {
+            if (palavras[i]==palavras[j]){
+                frequencias[i]++;
             }
-            
-    console.log(frequencias);
-    return palavras;
+        }
     }
+    console.log(frequencias);
+    
+    return palavras;
+}
