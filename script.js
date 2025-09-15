2ae6312fdc41a6a91a099dc04e385b62a5b63cef
mariastaudt <maria.staudt@escola.pr.gov.br>
2025-09-15 08:53:38 -0300

    bk

--- script.js
+++ script.js
@@ -14,21 +14,17 @@ campoResultado.textContent = palavrasChave.join(", ");
 
 }
 
-function processaTexto(texto) {
-let palavras = texto.split(/\P{L}+/u);
-
-let frequencias = {};
-for(let i of palavras){
-frequencias [i] = 0;
-for (let j of palavras){
-       if (i == j){
-        frequencias[i]++;
-       }
+function processa Texto (texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    
+    let frequencias = [];
+    for(let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias [i]++;
+            }
+            
+    console.log(frequencias);
+    return palavras;
     }
-}    
-
-console.log(frequencias);
-
-return palavras;
-
-}
