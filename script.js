a296e5737840a151e5fbd045f936cc8ebf7e9683
mariastaudt <maria.staudt@escola.pr.gov.br>
2025-09-15 08:33:18 -0300

    dfgh

--- script.js
+++ script.js
@@ -15,8 +15,15 @@ campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
-
 let palavras = texto.split(/\P{L}+/u);
+
+let frequencias = {};
+for(let i in palavras){
+frequencias [i] = 0;
+
+}    
+
+console.log(frequencias);
 
 return palavras;
 
