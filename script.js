b4628ed55ffc6f6f52a3c7f47c6133f92efc79b6
mariastaudt <maria.staudt@escola.pr.gov.br>
2025-09-15 07:55:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const botaMostraPalavras = document.querySelector("#botao-palavrachave*):
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
-function mostraPalavrasChave(){
-const texto = document querySelector (*#entrada-de-texto").value;
-const campoResultado = document.querySelector("#resultado-palavrachave);
-const palavrasChave = processaTexto(texto):
-campoResultado.textContent = palavrasChave.join(",");
+
+function mostraPalavrasChave() {
+
+const texto = document.querySelector("#entrada-de-texto").value;
+
+const campoResultado = document.querySelector("#resultado-palavrachave");
+
+const palavrasChave = processaTexto(texto);
+
+campoResultado.textContent = palavrasChave.join(", ");
+
 }
-fuction processaTexto(texto){
- let palavra = texto.split(/\P{L}+/u);
+
+function processaTexto(texto) {
+
+let palavras = texto.split(/\P{L}+/u);
 
 return palavras;
+
 }
