1b843243da3ed9fea5bae51bc46ae9001fa4555c
mariastaudt <maria.staudt@escola.pr.gov.br>
2025-09-15 08:49:44 -0300

    fgh

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function processaTexto(texto) {
 let palavras = texto.split(/\P{L}+/u);
 
 let frequencias = {};
-for(let i in palavras){
+for(let i of palavras){
 frequencias [i] = 0;
-for (let j in palavras){
-       if (palavras[i]=palavras[j](
+for (let j of palavras){
+       if (i == j){
         frequencias[i]++;
        }
     }
